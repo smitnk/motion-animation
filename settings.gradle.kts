6733d1e26616d1e8ccac8a7d6ec5e2bb05e6f0ab
smitnk <sumitnaikna29@gmail.com>
2026-09-18 15:01:35 +0530

    fix: resolve Android Gradle plugin from Google Maven

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.application") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "Motion Animation"
 include(":app")
