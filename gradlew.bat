c908d9b6265a86497a95431a89e2575f572db054
smitnk <sumitnaikna29@gmail.com>
2026-09-18 19:44:11 +0530

    build: add Windows Gradle wrapper launcher

---
+++ gradlew.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+set APP_HOME=%~dp0
+set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+
+if defined JAVA_HOME (
+  set JAVACMD=%JAVA_HOME%\bin\java.exe
+) else (
+  set JAVACMD=java.exe
+)
+
+"%JAVACMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+endlocal
